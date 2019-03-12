43ccfc266b3dd54a871233df5d58f2dd574d42c7
kimdonghyunn <crackmaker@naver.com>
2019-03-12 20:36:33 +0900

    practice03-1-4 연습03-1-4

---
+++ SimpleProject/SimpleProject/practice03_1_4.c
@@ -0,0 +1,9 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int i = 0, j = 0;
+	printf("두 정수를 입력하세요.\n");
+	scanf_s("%d %d",&i,&j);
+	printf("%d를 %d로 나누면 \n몫:%d\n나머지:%d", i, j, i / j, i%j);
+	return 0;
+}
