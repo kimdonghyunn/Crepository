21260913c60d235be56f62c35e6c8e3d87a0f105
kimdonghyunn <crackmaker@naver.com>
2019-03-12 20:23:04 +0900

    practice 03-1-1 연습 03-1-1

---
+++ SimpleProject/SimpleProject/practice03-1-1.c
@@ -0,0 +1,9 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int i=0, j=0;
+	printf("정수를 두개 입력하세요.");
+	scanf_s("%d %d", &i, &j);
+	printf("두 수 %d와 %d의 뺄셈:%d\n두 수 %d와 %d의 곱셈:%d\n",i,j,i - j,i,j,i*j);
+	return 0;
+}
