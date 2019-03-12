d88831c80be01b00cbac18f85c12e7fc6948bcff
kimdonghyunn <crackmaker@naver.com>
2019-03-12 23:25:02 +0900

    practice05-1-2 연습05-1-2

---
+++ SimpleProject/SimpleProject/prcatice05_1_2.c
@@ -0,0 +1,9 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	double i = 0, j = 0;
+	printf("두개의 실수를 입력하세요.\n");
+	scanf_s("%lf %lf", &i, &j);
+	printf("%lf+%lf=%lf\n%lf-%lf=%lf\n%lf×%lf=%lf\n%lf÷%lf=%lf\n",i,j,i+j,i,j,i-j,i,j,i*j,i,j,i/j);
+	return 0;
+}
