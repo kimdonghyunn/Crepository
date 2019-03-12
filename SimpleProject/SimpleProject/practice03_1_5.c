feedba95c2d9f49f89da38f3eafd09d8b44d31f5
kimdonghyunn <crackmaker@naver.com>
2019-03-12 20:44:00 +0900

    practice03-1-5 연습03-1-5

---
+++ SimpleProject/SimpleProject/practice03_1_5.c
@@ -0,0 +1,9 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int i = 0, j = 0, k = 0;
+	printf("정수를 세개 입력하세요.\n");
+	scanf_s("%d %d %d", &i, &j, &k);
+	printf("(%d-%d)×(%d+%d)×(%d%%%d)=%d", i, j, j, k, k, i, (i - j)*(j + k)*(k%i));
+	return 0;
+}
