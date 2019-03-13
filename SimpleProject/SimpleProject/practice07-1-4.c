db1d5f777f3fb70d2eb15fc98e020d814fa54f0f
kimdonghyunn <crackmaker@naver.com>
2019-03-13 21:37:12 +0900

    practice07-1-4 연습07-1-4

---
+++ SimpleProject/SimpleProject/practice07-1-4.c
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int i = 0, j = 9;
+	printf("정수를 하나 입력하세요.\n");
+	scanf_s("%d", &i);
+	while (j>0) {
+		printf("%d×%d=%d\n", i, j, i*j);
+		j--;
+	}
+	return 0;
+}
