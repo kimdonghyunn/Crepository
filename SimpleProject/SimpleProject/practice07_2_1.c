e5a277efd6168414f09b5ad74ba569f4de77590f
kimdonghyunn <crackmaker@naver.com>
2019-03-13 23:44:54 +0900

    practice07-2-1 연습07-2-1

---
+++ SimpleProject/SimpleProject/practice07_2_1.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int i = 0, j = 0, sum = 0;
+	while (i<5) {
+		j = 0;
+		while (j<=0) {
+			printf("정수를 입력하세요. 0이하의 숫자를 입력하면 다시 입력해야합니다.\n");
+			scanf("%d", &j);
+		}
+		sum += j;
+		i++;
+	}
+	printf("%d", sum);
+}
