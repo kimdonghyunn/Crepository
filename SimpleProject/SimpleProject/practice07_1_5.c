e4361bbfeea4eec4f4a3e3b5790777ff342c47e1
kimdonghyunn <crackmaker@naver.com>
2019-03-13 21:46:33 +0900

    practice07-1-5 연습07-1-5

---
+++ SimpleProject/SimpleProject/practice07_1_5.c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int i = 0, j = 0, k = 0, sum = 0;
+	double avg = 0;
+	printf("몇개의 정수를 입력 받겠습니까?\n");
+	scanf_s("%d", &i);
+	while (j<i) {
+		printf("정수를 입력하세요.\n");
+		scanf_s("%d", &k);
+		sum += k;
+		j++;
+	}
+	avg = sum / i;
+	printf("%f\n", avg);
+	return 0;
+}
