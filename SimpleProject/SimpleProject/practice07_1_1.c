688dc5a56939976e4a25626900384e7af51d30d5
kimdonghyunn <crackmaker@naver.com>
2019-03-13 21:10:48 +0900

    practice07-1-1 연습07-1-1

---
+++ SimpleProject/SimpleProject/practice07_1_1.c
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int i = 0, j = 0;
+	printf("정수를 입력하세요.\n");
+	scanf("%d", &i);
+	while (j < i) {
+		printf("Hello world!%d\n", j);
+		j++;
+	}
+	return 0;
+}
