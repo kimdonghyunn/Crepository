2bf37a9f472b34dfe95749ddfdf28eacefda4ec5
kimdonghyunn <crackmaker@naver.com>
2019-03-15 00:12:23 +0900

    practice07-3-3 연습07-3-3

---
+++ SimpleProject/SimpleProject/practice07_3_3.c
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int dan = 2, num = 0;
+	do {
+		num = 1;
+		do {
+			printf("%d×%d=%d\n",dan,num,dan*num );
+			num++;
+		} while (num<10);
+		dan++;
+	} while (dan<10);
+	return 0;
+}
