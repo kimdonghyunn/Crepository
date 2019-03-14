fbb24e9a2c418ba6ec5a821a110954f9def1d1b0
kimdonghyunn <crackmaker@naver.com>
2019-03-14 23:19:10 +0900

    practice07-3-1-2 연습07-3-1-2

---
+++ SimpleProject/SimpleProject/practice07_3_1_2.c
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int i = 0, sum = 0;
+	printf("while문에 사용할 정수를 하나 입력하세요.\n");
+	scanf("%d", &i);
+	while (i != 0) {
+		printf("정수를 입력하세요(0 to quit)\n");
+		scanf("%d", &i);
+		sum += i;
+	}
+	printf("합계:%d", sum);
+	return 0;
+}
