cb4ce2ba543b6d4c62212a02d6a3dc11dc98e34b
kimdonghyunn <crackmaker@naver.com>
2019-03-14 23:12:35 +0900

    practice07-3-1-1 연습07-3-1-1

---
+++ SimpleProject/SimpleProject/practice07_3_1_1.c
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int i =1, sum = 0;
+	while (i != 0) {
+		printf("정수 입력(0 to quit)\n");
+		scanf("%d", &i);
+		sum += i;
+	}
+	printf("합계:%d", sum);
+	return 0;
+}
