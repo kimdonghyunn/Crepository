d9703f82b3a3db155e27ed6e30e8d1bf6e6d173b
kimdonghyunn <crackmaker@naver.com>
2019-03-16 12:27:40 +0900

    practice07-4-1 연습07-4-1

---
+++ SimpleProject/SimpleProject/practice07_4_1.c
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int num = 0,num2=0, i, sum = 0;
+	printf("두 개의 정수를 입력하세요.\n");
+	scanf_s("%d %d", &num, &num2);
+	for (; num <=num2; num++ ) {
+		sum += num;
+	}
+	printf("합계:%d", sum);
+	return 0;
+}
