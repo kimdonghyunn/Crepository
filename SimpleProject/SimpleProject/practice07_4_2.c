20b0a73982dcb5f2ec9e5e1f0ea2beec5b5c7236
kimdonghyunn <crackmaker@naver.com>
2019-03-16 12:33:57 +0900

    practice07-4-2 연습07-4-2

---
+++ SimpleProject/SimpleProject/practice07_4_2.c
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int num = 0, i, sum = 1;
+	printf("계승을 위한 정수를 입력하세요.\n");
+	scanf_s("%d", &num);
+	for (i = 1; i < num+1; i++) {
+		sum *= i;
+	}
+	printf("합계:%d", sum);
+	return 0;
+}
