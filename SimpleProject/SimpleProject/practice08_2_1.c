a8b5c0f8120d2fe3fa4ad2d3fa439d03e7433906
kimdonghyunn <crackmaker@naver.com>
2019-03-16 14:21:11 +0900

    practice08-2-1 연습08-2-1

---
+++ SimpleProject/SimpleProject/practice08_2_1.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int num = 0, dan = 0;
+	for (dan = 2; dan < 9; dan++) {
+		if (dan % 2 == 0) {
+			for (num = 1; num <= dan; num++) {
+				printf("%d*%d=%d\n", dan, num, dan*num);
+			}
+		}
+		else
+			continue;
+	}
+	return 0;
+}
