0af2e00d8a3de74d43b6851f0b0b9e15bb658a88
kimdonghyunn <crackmaker@naver.com>
2019-03-16 14:32:18 +0900

    practice08-2-2 연습08-2-2

---
+++ SimpleProject/SimpleProject/practice08_2_2.c
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int i = 0, j = 0;
+	for (i=0;i<10;i++) {
+		for (j=0;j<10;j++) {
+			if (((i * 10) + j) + ((j * 10) + i) == 99) {
+				printf("%d%d+%d%d=99\n", i, j, j, i);
+			}
+			else
+				continue;
+
+		}
+	}
+	return 0;
+}
