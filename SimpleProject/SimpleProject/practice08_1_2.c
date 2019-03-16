9a2894a55f38557cabd732c9eb11517ca50df981
kimdonghyunn <crackmaker@naver.com>
2019-03-16 13:30:29 +0900

    practice08-1-2 연습08-1-2

---
+++ SimpleProject/SimpleProject/practice08_1_2.c
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int i = 0, j = 0;
+	printf("뺄셈을 할 정수를 두개 입력하세요. 큰 값에서 작은 값이 빼집니다.\n");
+	scanf_s("%d %d", &i, &j);
+	if (i < j) 
+		printf("%d-%d=%d\n", j, i, j - i);
+	
+	else
+		printf("%d-%d=%d\n", i, j, i-j);
+	return 0;
+}
