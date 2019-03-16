b93613ac66473320ccb72bd700064b03abf0dfe2
kimdonghyunn <crackmaker@naver.com>
2019-03-16 13:38:50 +0900

    practice08-1-3 연습08-1-3

---
+++ SimpleProject/SimpleProject/practice08_1_3.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int kor = 0, eng = 0, mat = 0;
+	printf("국어,영어,수학의 점수를 차례대로 입력하세요.\n");
+	scanf_s("%d %d %d", &kor, &eng, &mat);
+	int avg = (kor + eng + mat) / 3;
+	if (avg >= 90)
+		printf("성적:A");
+	else if (avg >= 80)
+		printf("성적:B");
+	else if (avg >= 70)
+		printf("성적:C");
+	else if (avg >= 50)
+		printf("성적:D");
+	else
+		printf("성적:F");
+
+	return 0;
+}
