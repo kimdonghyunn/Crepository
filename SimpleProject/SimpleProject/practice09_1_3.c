9eaa2abcb97e6af852c1ecd15e820c6ad76eabad
kimdonghyunn <crackmaker@naver.com>
2019-03-19 23:07:50 +0900

    practice09-1-3 제일어려웠음 간단한 메카니즘을 이해못했고, 생각하지도못함 결국 답안지를 봄 연습09-1-3

---
+++ SimpleProject/SimpleProject/practice09_1_3.c
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+
+int fibo(int num) {
+	int n1=0,n2=1,i,n3 ;
+	if (num == 1) {
+		printf("%d", n1);
+	}
+	else if (num == 2) {
+		printf("%d %d", n1, n2);
+	}
+	else
+		printf("%d %d", n1, n2);
+	for (i = 1; i < num-1;i++) {
+		n3 = n1 + n2;
+		printf(" %d",n3);
+		n1 = n2;
+		n2 = n3;
+	}
+}
+
+int main(void) {
+	int num = 0;
+	printf("나열할 피보나치 수열의 값을 입력하세요.\n");
+	scanf_s("%d", &num);
+	fibo(num);
+	return 0;
+}
