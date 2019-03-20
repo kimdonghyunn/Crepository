7b036dabaaea6eb054ef3add1adb695218cd0721
kimdonghyunn <crackmaker@naver.com>
2019-03-20 21:35:55 +0900

    challenge-1 도전-1

---
+++ SimpleProject/SimpleProject/challenge_1.c
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+
+int TenToSixteen(int num) {
+	printf("16진수로 바꿀 10진수를 입력하세요.");
+	scanf("%d", &num);
+	printf("10진수 %d를 16진수로 바꾸면 %x입니다.", num, num);
+	return num;
+}
+
+int main(void) {
+	int num = 0;
+	TenToSixteen(num);
+	return 0;
+}
