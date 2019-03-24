b3a14ded5af71ed8af4c88a06ab92fe92032922a
kimdonghyunn <crackmaker@naver.com>
2019-03-25 01:06:12 +0900

    challenge-5 도전-5 소수구하기..어려웠음

---
+++ SimpleProject/SimpleProject/challenge-5.c
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+
+void prime(void) {
+	int i = 0, j = 1, k = 1, sum = 0;
+		for (i=0,j = 2;i<100; j++) {
+			sum = 0;
+			for (k = 1; k < j+1; k++) {
+				if (j%k == 0) {
+					sum++;
+				}
+			}
+			if (sum == 2) {
+				printf("%d ", j);
+				i++;
+				continue;
+			}
+		}
+	}
+
+int main(void) {
+	prime();
+	return 0;
+}
