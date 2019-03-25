54a4be24e93855c980347af1ebd70860c67523d8
kimdonghyunn <crackmaker@naver.com>
2019-03-25 22:00:46 +0900

    challenge-7 도전-7

---
+++ SimpleProject/SimpleProject/challenge_7.c
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+
+void ek(void) {
+	int n = 0,k = 0,sum=0;
+	printf("상수 n 입력: ");
+	scanf("%d", &n);
+	for ( k = 0,sum=2;sum<=n; k++){
+		sum *= 2;
+	}
+	printf("공식을 만족하는 k의 최대값은 %d", k);
+}
+
+int main(void) {
+	ek();
+	return 0;
+}
