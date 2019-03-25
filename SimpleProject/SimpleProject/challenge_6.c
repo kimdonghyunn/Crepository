03534367c4bf5b8de9b96bef6d2a2e173ae6a17c
kimdonghyunn <crackmaker@naver.com>
2019-03-25 20:23:27 +0900

    challenge-6 도전-6 입력받은 초를 시,분,초 형태로 변환하기

---
+++ SimpleProject/SimpleProject/challenge_6.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+
+void secToTime(void) {
+	int sec=0 , min=0 , hou=0 ;
+	printf("초(second) 입력\n");
+	scanf("%d", &sec);
+	hou = sec / 3600;
+	min = (sec - (3600 * hou)) / 60;
+	sec = sec - (3600 * hou) - (60 * min);
+	printf("[h:%d, m:%d, s:%d]", hou, min, sec);
+}
+int main(void) {
+	secToTime();
+	return 0;
+}
