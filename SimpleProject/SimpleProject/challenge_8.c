e87ab3c226bcb55f1700cd97d87ccbeae1521f24
kimdonghyunn <crackmaker@naver.com>
2019-03-25 23:33:11 +0900

    challenge-8 도전-8 2의n승구하기

---
+++ SimpleProject/SimpleProject/challenge_8.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+
+int Factorial(int n) {
+	if (n == 0) {
+		return 1;
+	}
+	else
+		return 2 * Factorial(n - 1);
+}
+int main(void) {
+	int n = 0;
+	printf("정수입력:");
+	scanf("%d", &n);
+	printf("2의 %d승은 %d", n, Factorial(n));
+}
