37f5d1f79a4ae3cbb9c4da2e9f563e34e2adfa26
kimdonghyunn <crackmaker@naver.com>
2019-03-27 19:10:48 +0900

    practice11-2 연습11-2 char형 1차원 배열을 문장으로 초기화하고 내용출력하기

---
+++ SimpleProject/SimpleProject/practice11_2.c
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int i, arlen;
+	char arraay[] = {"\"Good Time\""};
+	printf("배열 사이즈:%d\n", sizeof(arraay));
+	arlen = sizeof(arraay) / sizeof(char);
+	for (i = 0; i < arlen; i++){
+	printf("%c", arraay[i]);
+	}
+	return 0;
+}
