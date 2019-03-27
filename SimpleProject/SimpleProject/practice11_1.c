9342103324ac01ace7459800e7e525a1610b40f6
kimdonghyunn <crackmaker@naver.com>
2019-03-27 18:56:44 +0900

    practice11-1 연습11-1 길이가 5인 int형배열을 선언하고 5개의 정수를 입력받아, 최댓값,최소값,배열요소의 총합 출력하기

---
+++ SimpleProject/SimpleProject/practice11_1.c
@@ -0,0 +1,33 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int arrayy[5];
+	int i, max, min,sum=0;
+	for (i = 0; i < 5; i++) {
+		printf("%d번째 정수를입력하세요.\n", i + 1);
+		scanf("%d", &arrayy[i]);
+	}
+	for (i = 0; i < 4; i++) {
+		if (arrayy[0] > arrayy[i + 1]) {
+			max = arrayy[0];
+		}
+		else {
+			max = arrayy[i + 1];
+		}
+	}
+	printf("최대값:%d\n", max);
+	for (i = 0; i < 4; i++) {
+		if (arrayy[0] > arrayy[i + 1]) {
+			min = arrayy[i + 1];
+		}
+		else {
+			min = arrayy[0];
+		}
+	}
+	printf("최소값:%d\n", min);
+	for (i = 0; i < 5; i++) {
+		sum += arrayy[i];
+	}
+	printf("입력된 정수의 총합:%d\n", sum);
+	return 0;
+}
