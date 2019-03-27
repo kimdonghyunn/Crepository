45afe25674331135205148e921e33d0987e7b8ce
kimdonghyunn <crackmaker@naver.com>
2019-03-27 20:21:10 +0900

    practice11-2-1 연습11-2-1 char형 문자열을 입력받아 문자열의 길이 계산하여 출력하기

---
+++ SimpleProject/SimpleProject/practice11_2_1.cpp
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	char eng[15];
+	int i;
+	printf("영단어를 입력하세요.\n");
+	scanf("%s", eng);
+	for (i = 0; eng[i] != '\0'; i++) {
+
+	}
+	printf("영단어의 길이는 %d입니다.\n", i);
+	return 0;
+}
