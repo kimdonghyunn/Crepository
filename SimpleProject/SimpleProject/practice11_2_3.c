4bc72fa92b950176d369d3b0a71c920d8f12d46b
kimdonghyunn <crackmaker@naver.com>
2019-03-28 00:24:25 +0900

    practice11-2-3 연습11-2-3 어렵다

---
+++ SimpleProject/SimpleProject/practice11_2_3.c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	char eng[150];
+	char big=0;
+	int i;
+	printf("단어입력:");
+	scanf("%s", eng);
+	big = eng[0];
+	for (i = 1; eng[i] != '\0'; i++) {
+		if (big < eng[i]) {
+			big = eng[i];
+		}
+	}
+	printf("%c",big);
+	return 0;
+}
