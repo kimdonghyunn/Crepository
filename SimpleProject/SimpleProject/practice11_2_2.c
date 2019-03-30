122b448b02ed6609d5c1c11f5fb437482c85a0fa
kimdonghyunn <crackmaker@naver.com>
2019-03-31 00:19:15 +0900

    practice11-2-2 연습11-2-2 제일어려웠던문제. 영단어를 입력받아 뒤집기

---
+++ SimpleProject/SimpleProject/practice11_2_2.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	char temp ;
+	char voca[50];
+	int i = 0, len = 0;
+	printf("영단어를 입력하세요.\n");
+	scanf("%s", voca);
+	for (i = 0; voca[i] != '\0'; i++) {
+		len++;
+	}
+	for (i = 0; i < len / 2; i++) {
+		temp = voca[i];
+		voca[i] = voca[(len - i)-1];
+		voca[(len - i) - 1] = temp;
+	}
+	printf("%s", voca);
+	return 0;
+}
