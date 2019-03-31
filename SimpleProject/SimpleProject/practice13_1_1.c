823aa85065a3dc03e142d04822aae0cc57a792b2
kimdonghyunn <crackmaker@naver.com>
2019-03-31 16:31:34 +0900

    practice13-1-1 연습13-1-1 포인터를 이용한 배열의 접근

---
+++ SimpleProject/SimpleProject/practice13_1_1.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int i = 0;
+	int arr[5] = { 1,2,3,4,5 };
+	int * ptr = arr;
+	for (i = 0; i < 5; i++) {
+		*(ptr+i) += 2;
+	}
+	printf("%d %d %d %d %d\n", *ptr, *(ptr + 1), *(ptr + 2), *(ptr + 3), *(ptr + 4));
+	printf("%d %d %d %d %d\n", ptr[0], ptr[1], ptr[2], ptr[3], ptr[4]);
+	printf("%d %d %d %d %d\n", *(arr), *(arr+1),*(arr+2),*(arr+3),*(arr+4));
+	printf("%d %d %d %d %d\n", arr[0], arr[1], arr[2], arr[3], arr[4]);
+	return 0;
+}
