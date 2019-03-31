33410e6797f195593738461f0a49187c6e44e416
kimdonghyunn <crackmaker@naver.com>
2019-03-31 17:33:15 +0900

    practice13-1-4 연습13-1-4 주석부분은 왜 실행이 안되는건지?

---
+++ SimpleProject/SimpleProject/practice13_1_4.c
@@ -0,0 +1,24 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int arr[6] = { 1,2,3,4,5,6 };
+	int * fptr = &arr[0];
+	int * bptr = &arr[5];
+	int i = 0,temp=0;
+	for (i = 0; i < 3; i++) {
+		temp = *fptr;
+		*fptr = *bptr;
+		*bptr = temp;
+		fptr+=1;
+		bptr -= 1;
+	}
+	/*for (i = 0; i < 3; i++) {
+		temp = *(fptr+i);
+		*(fptr + i) = *(bptr+(6 - i));
+		*(bptr + (6 - i)) = temp;
+	}s
+	*/
+	for (i = 0; i < 6; i++) {
+		printf("%d ", arr[i]);
+	}
+}
