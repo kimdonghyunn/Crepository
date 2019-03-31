1007098f381cc6bd1f2710e285c2d40ae300cacd
kimdonghyunn <crackmaker@naver.com>
2019-03-31 18:02:30 +0900

    example of pointer array 포인터 배열의 예

---
+++ SimpleProject/SimpleProject/PointerArray.c
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>
+// 포인터 배열의 예
+int main(void) {
+	int num1 = 10, num2 = 20, num3 = 30;
+	int * arr[3] = { &num1,&num2,&num3 };
+	int i = 0;
+	for(i=0;i<3;i++){
+		printf("%d \n", *arr[i]);
+	}
+	return 0;
+}
