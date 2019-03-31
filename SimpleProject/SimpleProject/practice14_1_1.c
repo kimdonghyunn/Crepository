8cceb1c9babbab7f77b8067924810f4f6d664918
kimdonghyunn <crackmaker@naver.com>
2019-03-31 23:42:52 +0900

    practice14-1-1 연습14-1-1 callbyvalue&callbyreference

---
+++ SimpleProject/SimpleProject/practice14_1_1.c
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+
+int SquareByValue(int a) {
+	return a * a;
+}
+void SquareByReference(int * a){
+	(*a) *= (*a);
+}
+int main(void) {
+	int i = 2;
+	SquareByReference(&i);
+	printf("%d %d", SquareByValue(2), i);
+	return 0;
+}
