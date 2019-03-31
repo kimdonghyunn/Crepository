c67ce79805a7775e53f9c995632ecaca6e4d0339
kimdonghyunn <crackmaker@naver.com>
2019-03-31 16:59:30 +0900

    practice13-1-3 연습13-1-3 포인터를 이용한 배열의 접근3

---
+++ SimpleProject/SimpleProject/practice13_1_3.c
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int arr[5] = { 1,2,3,4,5 };
+	int * ptr = &arr[4];
+	int i = 0, sum = 0;
+	for (i = 0; i < 5; i++) {
+		sum += *(ptr--);
+	}
+	printf("%d", sum);
+	return 0;
+}
