3816c979ada9b1bcf32a5bab8a0dcd1f4ae01262
kimdonghyunn <crackmaker@naver.com>
2019-04-03 20:03:44 +0900

    practice13-1-4 연습13-1-4

--- SimpleProject/SimpleProject/practice13_1_4.c
+++ SimpleProject/SimpleProject/practice13_1_4.c
@@ -5,19 +5,18 @@ int main(void) {
 	int * fptr = &arr[0];
 	int * bptr = &arr[5];
 	int i = 0,temp=0;
-	for (i = 0; i < 3; i++) {
+	/*for (i = 0; i < 3; i++) {
 		temp = *fptr;
 		*fptr = *bptr;
 		*bptr = temp;
 		fptr+=1;
 		bptr -= 1;
-	}
-	/*for (i = 0; i < 3; i++) {
+	}*/
+	for (i = 0; i < 3; i++) {
 		temp = *(fptr+i);
-		*(fptr + i) = *(bptr+(6 - i));
-		*(bptr + (6 - i)) = temp;
-	}s
-	*/
+		*(fptr + i) = *(bptr- i);
+		*(bptr - i) = temp;
+	}
 	for (i = 0; i < 6; i++) {
 		printf("%d ", arr[i]);
 	}
