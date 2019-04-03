c41e49898faf20442ce809c60b46c02077866fd7
kimdonghyunn <crackmaker@naver.com>
2019-04-03 20:43:57 +0900

    challenge2-2 도전2-2

---
+++ SimpleProject/SimpleProject/challenge2_2.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+//사용자로부터 10진수 형태로 정수를 입력받고 이를 2진수로 변환해서 출력하는 프로그램
+int main(void) {
+	int num = 0,i=0,temp=0;
+	int easyjinsu[16];
+	printf("정수 입력:");
+	scanf("%d", &num);
+	for(i=0;i<16;i++){
+		easyjinsu[i] = num % 2;
+		num = num / 2;	
+	}
+	for (i = 0; i < 8; i++) {
+		temp = easyjinsu[i];
+		easyjinsu[i] = easyjinsu[15 - i];
+		easyjinsu[15 - i] = temp;
+	}
+	for(i=0;i<16;i++)
+		printf("%d", easyjinsu[i]);
+	return 0;
+}
