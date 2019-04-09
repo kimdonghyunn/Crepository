d767f835d0ff82fcf2db8cc0b5e18e1f4daa876f
kimdonghyunn <crackmaker@naver.com>
2019-04-09 23:52:25 +0900

    no message

---
+++ SimpleProject/SimpleProject/PopuResarch.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int villa[4][2];
+	int sum = 0, i = 0, j = 0;
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 2; j++) {
+			printf("%d층 %d호의 거주자수:", i + 1, j + 1);
+			scanf("%d", &villa[i][j]);
+		}
+	}
+	for (i = 0; i < 4; i++) {
+		sum = 0;
+		sum += villa[i][0];
+		sum += villa[i][1];
+		printf("%d층의 거주자수:%d\n", i+1, sum);
+	}
+	return 0;
+}
---
+++ SimpleProject/SimpleProject/practice16_1_3.c
@@ -0,0 +1,52 @@
+﻿#include<stdio.h>
+
+int arr[5][5];
+int sum = 0;
+void sungjukip(void) {
+	int i = 0, j = 0, sum = 0, sum2 = 0;
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			printf("%d번째 학생의 국어,영어,수학,국사 점수입력", i + 1);
+			scanf("%d", &arr[i][j]);
+		}
+	}
+}
+
+void studentsum(void) {
+	int i = 0, j = 0;
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			arr[i][4] += arr[i][j];
+			
+		}
+	}
+}
+
+void gwamoksum(void) {
+	int i = 0, j = 0;
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			arr[4][i] += arr[j][i];
+		}
+
+		sum += arr[4][i];
+	}
+}
+
+
+void chulruk(void) {
+	int i = 0, j = 0;
+	arr[4][4] = sum;
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 5; j++) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
+int main(void) {
+	sungjukip();
+	studentsum();
+	gwamoksum();
+	chulruk();
+}
